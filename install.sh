1f4eb8ee291ce8611a8a103bcb52dffa73802fe3
Jordi Barreda <jordibarreda98@gmail.com>
2026-06-29 11:49:22 +0200

    actualizacion install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ check_root(){
 install_dependencies(){
     log_info "Instalando dependencias del sistema..."
     apt update -qq
-    apt install -y apache2 libapache2-mod-proxy-html python3-venv python3-pip gunicorn jq &>/dev/null
+    apt install -y apache2 python3-venv python3-pip gunicorn jq &>/dev/null
     a2enmod proxy proxy_http &>/dev/null
     log_ok "Dependencias instaladas."
 }
